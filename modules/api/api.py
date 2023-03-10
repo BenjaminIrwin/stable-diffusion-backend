9119da3d60fe4f9407573694b686e8545eefc277
EC2 Default User <ec2-user@ip-172-31-30-130.eu-west-2.compute.internal>
2023-03-10 17:25:53 +0000

    api tweak

--- modules/api/api.py
+++ modules/api/api.py
@@ -164,6 +164,8 @@ class Api:
         return self.app.add_api_route(path, endpoint, dependencies=[Depends(self.auth)], **kwargs)
 
     def auth(self, api_key: APIKey = Depends(APIKeyHeader(name="api_key", auto_error=False))):
+        print('API KEY:')
+        print(api_key)
         if api_key:
             # Query firebase firestore database with api_key
             res = self.users_db.where('api_key', '==', api_key).get()
@@ -223,6 +225,9 @@ class Api:
         # Set prompt to random from prompts
         img2imgreq.prompt = random.choice(prompts)
         img2imgreq.negative_prompt = negative_prompt
+        print(img2imgreq.prompt)
+        print(img2imgreq.negative_prompt)
+        print(img2imgreq)
         if init_images is None:
             raise HTTPException(status_code=404, detail="Init image not found")
 
