7ef7d505e8c0f776a399c56aa366922f188177a3
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-03-17 14:46:29 +0000

    Adjusted padding

--- modules/processing.py
+++ modules/processing.py
@@ -942,6 +942,9 @@ class StableDiffusionProcessingImg2Img(StableDiffusionProcessing):
             if self.inpaint_full_res:
                 self.mask_for_overlay = image_mask
                 mask = image_mask.convert('L')
+                # Adjust inpaint_full_res_padding to image size
+                adjusted_padding = (self.inpaint_full_res_padding / 512) * mask.width
+                self.inpaint_full_res_padding = adjusted_padding
                 crop_region = masking.get_crop_region(np.array(mask), self.inpaint_full_res_padding)
                 crop_region = masking.expand_crop_region(crop_region, self.width, self.height, mask.width, mask.height)
                 x1, y1, x2, y2 = crop_region
