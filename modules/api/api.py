2fbb560b28bc80ae8f7d7151cf2c600948dc43d2
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-04-14 09:03:17 +0100

    Convert all init images off RGBA

--- modules/api/api.py
+++ modules/api/api.py
@@ -467,6 +467,8 @@ class Api:
         with self.queue_lock:
             p = StableDiffusionProcessingImg2Img(sd_model=shared.sd_model, **args)
             p.init_images = [decode_base64_to_image(x) for x in init_images]
+            # Make all init_images RGB
+            p.init_images = [x.convert('RGB') for x in p.init_images]
             p.scripts = script_runner
             p.outpath_grids = opts.outdir_img2img_grids
             p.outpath_samples = opts.outdir_img2img_samples
