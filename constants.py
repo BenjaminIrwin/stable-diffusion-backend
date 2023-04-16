175109b8ab7bd4904a71d6090976220bf1f74b8f
EC2 Default User <ec2-user@ip-172-31-22-8.eu-west-2.compute.internal>
2023-04-16 20:23:48 +0000

    control net api route

---
+++ constants.py
@@ -0,0 +1,14 @@
+fs_uid = 'qkdpzpiqnfl1'
+fs_url = 'https://lightsketch-a063f-default-rtdb.europe-west1.firebasedatabase.app'
+fs_sa_key = '/home/ec2-user/keys/fs_sa_key.json'
+aws_access_key_id = 'AKIAW3YIJRQ7653XVP6F'
+aws_secret_access_key = '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'
+
+orientation_override_words = ['angle', 'bend', 'crane', 'dip', 'direct', 'examine', 'eye', 'face', 'fixate', 'focus', 'gaze', 'glare',
+             'glimpse', 'inspect', 'lean', 'leer', 'look', 'lower', 'notice', 'observe', 'overlook', 'peer', 'point',
+             'raise', 'regard', 'scan', 'see', 'shift', 'spot', 'spy', 'squint', 'stare', 'study', 'survey', 'take in',
+             'turn', 'twist', 'view', 'watch']
+
+
+negative_prompts = {'people': 'disfigured, poorly drawn face, portrait, lowres, bad anatomy, cropped, worst quality, ugly, deformed, odd, fat, morphing, (((deformed))), mutation, mutated, (extra_limb), (ugly), (poorly hands), messy broken legs, decay'}
+prompt_modifiers = {'people': 'award winning photography, high resolution, best quality, extremely detailed text'}
