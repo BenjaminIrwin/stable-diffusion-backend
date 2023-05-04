8bbc8859ae467eb398a07a7215e24156f85a6e59
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-05-04 08:51:37 +0100

    Changed prompt_gen.py

--- prompt_gen.py
+++ prompt_gen.py
@@ -1,16 +1,20 @@
 from constants import orientation_override_words
 from word_present import word_present
 from constants import negative_prompts, prompt_modifiers
+import time
 
 
 def people_prompt_gen(action, age, sex, clothing):
+    start_time = time.time()
     orientation_specified = word_present(action, orientation_override_words)
     if not orientation_specified:
-        neg_prompt = 'facing the camera, ' + negative_prompts['people']
-        prompt_mod = prompt_modifiers['people'] + ', facing away'
+        neg_prompt = 'looking at the camera, ' + negative_prompts['people']
+        prompt_mod = prompt_modifiers['people'] + ', looking away'
     else:
         neg_prompt = negative_prompts['people']
         prompt_mod = prompt_modifiers['people']
+    end_time = time.time()
+    print('TIME TO CHECK PROMPT: ' + str(end_time - start_time))
 
     prompt = age + ' ' + sex + ' wearing ' + clothing + ' ' + action + ', ' + prompt_mod
     print('PROMPT:')
