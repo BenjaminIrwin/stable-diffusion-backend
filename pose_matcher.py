e82868e0522c22cf5ecac90325f3d8d77179ed91
BenjaminIrwin <benjamintdirwin@gmail.com>
2023-05-05 15:48:40 +0100

    Transformation bug fix

--- pose_matcher.py
+++ pose_matcher.py
@@ -55,13 +55,12 @@ def apply_transformations(image, transformations):
             pad_amount = transformation[1]
             # Compute the new size of the image, including the padding.
             new_size = (
-                image.width + pad_amount[0] + pad_amount[2],
-                image.height + pad_amount[1] + pad_amount[3]
+                int(image.width + pad_amount[0] + pad_amount[2]),
+                int(image.height + pad_amount[1] + pad_amount[3])
             )
 
             # Create a new image with the desired size and a transparent background.
-            mode = 'RGBA' if 'A' in image.getbands() else 'RGB'
-            new_image = Image.new(mode, new_size, (0, 0, 0, 0))
+            new_image = Image.new('RGBA', new_size, (0, 0, 0, 0))
 
             # Paste the original image onto the new image, using the padding amounts to determine the position.
             new_image.paste(image, (pad_amount[0], pad_amount[1]))
